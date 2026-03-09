215a37af8a3cf5b4629668a0ebea5f44c104a026
bi7-salt <yuya.shiozaki9@gmail.com>
2026-03-09 14:24:56 +0900

    add catecory

--- main.go
+++ main.go
@@ -15,19 +15,24 @@ func main() {
 	var err error
 	db,err = sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)/todo_app?parseTime=true")
 	if err != nil {
-		log.Fatal("DB接続エラー:", err)
+		log.Fatal("DB error:", err)
 	}
 	defer db.Close()
 
 	if err = db.Ping(); err != nil {
-		log.Fatal("DB接続エラー:", err)
+		log.Fatal("DB error:", err)
 	}
-	fmt.Println("MySQLに接続成功")
+	fmt.Println("db connected")
+
+	db.Exec("DROP TABLE IF EXISTS tasks")
 
 	todoTableSQL := `
     CREATE TABLE IF NOT EXISTS tasks (
         id INT AUTO_INCREMENT PRIMARY KEY,
-        title VARCHAR(255) NOT NULL
+        title VARCHAR(255) NOT NULL,
+		categorize VARCHAR(255) NOT NULL,
+		done BOOLEAN NOT NULL DEFAULT FALSE,
+		memo TEXT
     );`
     _, err = db.Exec(todoTableSQL)
     if err != nil {
@@ -43,19 +48,20 @@ func main() {
 
 func add(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Query().Get("title")
+	categorize := r.URL.Query().Get("categorize")
+	memo := r.URL.Query().Get("memo")
 
-	_, err := db.Exec("INSERT INTO tasks (title) VALUES (?)", title)
+	_, err := db.Exec("INSERT INTO tasks (title, categorize, memo) VALUES (?, ?, ?)", title, categorize, memo)
 	if err != nil {
 		fmt.Printf("Added failed: %v\n", err)
 		fmt.Fprintf(w, "Added failed: %v\n", err)
 		return
 	}
-	fmt.Printf("Added: %s\n", title)
-	fmt.Fprintf(w, "Added: %s\n", title)
+	fmt.Fprintf(w, "Added: %s (Categorize: %s, Memo: %s)\n", title, categorize, memo)
 }
 
 func list(w http.ResponseWriter, r *http.Request) {
-	rows,err := db.Query("SELECT id, title FROM tasks")
+	rows,err := db.Query("SELECT id, title, categorize, memo FROM tasks")
 	if err != nil {
 		fmt.Fprintf(w,"Loading error: %v\n", err)
 		return
@@ -67,12 +73,15 @@ func list(w http.ResponseWriter, r *http.Request) {
 	for rows.Next() {
 		var id int
 		var title string
-		if err := rows.Scan(&id, &title); err != nil {
+		var categorize string
+		var memo string
+		
+		if err := rows.Scan(&id, &title, &categorize, &memo); err != nil {
 			fmt.Fprintf(w, "Loading error: %v\n", err)
 			return
 		}
 
-		fmt.Fprintf(w, "%d: %s\n", id, title)
+		fmt.Fprintf(w, "%d: %s (Categorize: %s, Memo: %s)\n", id, title, categorize, memo)
 	}
 
 	if err := rows.Err(); err != nil {
